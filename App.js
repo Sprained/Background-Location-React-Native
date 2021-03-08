fe88f83825093a2258a2a34fbe155b1e63e210b6
Gabriel Resende <gabriel.almeida.resende@gmail.com>
2021-03-08 15:24:49 -0300

    Primeiro commit

--- App.js
+++ App.js
@@ -1,12 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, View } from 'react-native';
+import * as Permissions from 'expo-permissions';
+import * as Location from 'expo-location';
+import * as TaskManager from 'expo-task-manager';
+import axios from 'axios';
+
+const LOCATION_TASK_NAME = 'background-location-task';
 
 export default function App() {
+  const [permission, askForPermission] = Permissions.usePermissions(Permissions.LOCATION, { ask: true });
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  // if (!permission || permission.status !== 'granted') {
+  //   return (
+  //     <View>
+  //       <Text>Permission is not granted</Text>
+  //       <Button title="Grant permission" onPress={askForPermission} />
+  //     </View>
+  //   );
+  // }
+
+  useEffect(() => {
+    onPress()
+  }, []);
+
+  onPress = async () => {
+    const { status } = await Location.requestPermissionsAsync();
+    if (status === 'granted') {
+      await Location.startLocationUpdatesAsync(LOCATION_TASK_NAME, {
+        accuracy: Location.Accuracy.High,
+        distanceInterval: 0,
+        timeInterval: 100,
+        pausesUpdatesAutomatically: false,
+        // foregroundService: {
+        //   // notificationTitle: 'Your title',
+        //   // notificationBody: 'Notification Body'
+        // },
+      });
+    }
+  };
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      
     </View>
   );
 }
@@ -18,4 +57,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+});
+
+TaskManager.defineTask(LOCATION_TASK_NAME, async ({ data: { locations }, error }) => {
+  if (error) {
+    // check `error.message` for more details.
+    return;
+  }
+
+  await axios.post('http://10.0.0.122:3333/teste', {
+    teste: locations[0]
+  })
 });
